abb6c78e5ef43ae566e520199c84fbcbad2aae09
ducanh170920 <ducanhchelseafc@gmail.com>
2020-11-04 09:01:51 +0700

    home

--- store/views.py
+++ store/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     context = {}
     return render(request,'store/index.html',context)
+def home(request):
+    context = {}
+    return render(request,'store/home.html',context)
